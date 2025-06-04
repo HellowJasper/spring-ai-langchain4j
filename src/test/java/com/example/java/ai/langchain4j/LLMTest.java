f3fcffc024ef4752e932d64e4ad5a0e781848ad7
Jasper <1938921378@qq.com>
2025-06-04 14:32:25 +0800

    使用ollama接入deepseek1.5b同时整合阿里百炼平台

--- src/test/java/com/example/java/ai/langchain4j/LLMTest.java
+++ src/test/java/com/example/java/ai/langchain4j/LLMTest.java
@@ -1,5 +1,8 @@
 package com.example.java.ai.langchain4j;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +24,39 @@ public class LLMTest {
     }
 
     @Autowired
-    private OpenAiChatModel openAiChatModel;
+    private OpenAiChatModel  openAiChatModel;
 
     @Test
     public void testSpringBoot() {
         String answer = openAiChatModel.chat("我是谁");
         System.out.println(answer);
 
+    }
+
+    /**
+     * ollama接入
+     */
+    @Autowired
+    private OllamaChatModel ollamaChatModel;
+
+    @Test
+    public void testOllama() {
+        //向模型提问
+        String answer = ollamaChatModel.chat("你好");
+        //输出结果
+        System.out.println(answer);
+    }
+    /**
+     * 通义千问大模型
+     */
+    @Autowired
+    private QwenChatModel qwenChatModel;
+    @Test
+    public void testDashScopeQwen() {
+        //向模型提问
+        String answer = qwenChatModel.chat("你好");
+        //输出结果
+        System.out.println(answer);
     }
 
 }
