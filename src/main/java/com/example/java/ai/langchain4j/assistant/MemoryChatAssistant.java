899791b88d4f2715946db3e7fa736cf886186866
Jasper <1938921378@qq.com>
2025-06-05 23:53:16 +0800

    增加了用户提示词功能

--- src/main/java/com/example/java/ai/langchain4j/assistant/MemoryChatAssistant.java
+++ src/main/java/com/example/java/ai/langchain4j/assistant/MemoryChatAssistant.java
@@ -1,5 +1,6 @@
 package com.example.java.ai.langchain4j.assistant;
 
+import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import org.apache.commons.lang3.ClassUtils;
@@ -12,5 +13,6 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 )
 public interface MemoryChatAssistant {
 
+    @UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{it}}") //{{it}}表示传进来的用户消息
     String chat(String message);
 }
--- src/test/java/com/example/java/ai/langchain4j/PromptTest.java
+++ src/test/java/com/example/java/ai/langchain4j/PromptTest.java
@@ -1,5 +1,6 @@
 package com.example.java.ai.langchain4j;
 
+import com.example.java.ai.langchain4j.assistant.MemoryChatAssistant;
 import com.example.java.ai.langchain4j.assistant.SeparateChatAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,16 @@ public class PromptTest {
 private SeparateChatAssistant separateChatAssistant;
 @Test
 public void testSystemMessage() {
+
     String answer = separateChatAssistant.chat(3, "今天是几号");
     System.out.println(answer);
 }
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+    @Test
+    public void testUserMessage() {
+        String answer = memoryChatAssistant.chat("这是河南话？");
+        System.out.println(answer);
+    }
 
 }
